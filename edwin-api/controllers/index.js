1bb6a2bfe7c1697664473e8ee45b0daef64ec359
Ben Dorton <ben.dorton@hughes.com>
2020-04-01 14:41:06 -0600

    removed server calls & files

--- edwin-api/controllers/index.js
+++ edwin-api/controllers/index.js
@@ -1,13 +1,11 @@
 const AdminController = require('./AdminController');
 const GeneralController = require('./GeneralController');
 const NotificationController = require('./NotificationController');
-const ServerController = require('./ServerController');
 const SiteAdminController = require('./SiteAdminController');
 
 module.exports = {
   AdminController,
   GeneralController,
   NotificationController,
-  ServerController,
   SiteAdminController,
 };
--- edwin-api/services/index.js
+++ edwin-api/services/index.js
@@ -1,13 +1,11 @@
 const AdminService = require('./AdminService');
 const GeneralService = require('./GeneralService');
 const NotificationService = require('./NotificationService');
-const ServerService = require('./ServerService');
 const SiteAdminService = require('./SiteAdminService');
 
 module.exports = {
   AdminService,
   GeneralService,
   NotificationService,
-  ServerService,
   SiteAdminService,
 };
