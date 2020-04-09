9ef9cc9c17c1d772a1becb3caec8ea211584db82
Ben Dorton <ben.dorton@hughes.com>
2020-04-08 20:47:41 -0600

    docker compose vue setup

--- edwin-frontend/src/api/http-common.js
+++ edwin-frontend/src/api/http-common.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
-let baseIP = process.env.BASE_URL;
+let baseIP = process.env.VUE_APP_BASE_URL;
 
 export const HTTP = axios.create({
   baseURL: baseIP,
-  //baseURL: `http://localhost:3030`,
-  //baseURL: `https://edwin.free.beeceptor.com`,
   headers: {
     //Authorization: 'Bearer {token}'
   }
